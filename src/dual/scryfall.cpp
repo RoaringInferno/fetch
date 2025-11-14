4737e083940d02cf82661dd21c0137fca7a3127b
RoaringInferno <jj2t17@gmail.com>
2025-11-14 16:50:46 -0600

    Banked Changes
    Started Dual module, configuring curl dependency

--- src/dual/scryfall.cpp
+++ src/dual/scryfall.cpp
@@ -1,11 +1,24 @@
 #include "scryfall.hpp"
 
+#include <string_view>
+#include <string>
+
+#include <simdjson/simdjson.h>
+#include <curl/curl.h>
+
+size_t write_file_callback(void* ptr, size_t size, size_t nmemb, void* userdata) {
+    std::ofstream* ofs = static_cast<std::ofstream*>(userdata);
+    size_t totalSize = size * nmemb;
+    ofs -> write(static_cast<char*>(ptr), totalSize);
+    return totalSize;
+}
+
 int download(
         scryfall_download_result_t &output,
-        std::filesystem::path directory,
-        progress_update_t update_progress
+        std::filesystem::path download_directory,
+        progress_update_t progress_function
         )
 {
     float status{0.0f};
-    update_progress(status);
+    progress_function(status);
 }
--- src/dual/scryfall.hpp
+++ src/dual/scryfall.hpp
@@ -5,8 +5,11 @@
 struct scryfall_download_result_t {
     std::filesystem::path json_path;
 };
+struct scryfall_download_opts_t {
+};
 int download(
         scryfall_download_result_t &output,
-        std::filesystem::path directory,
-        progress_update_t progress
+        std::filesystem::path download_directory,
+        progress_update_t progress_function,
+        scryfall_download_opts_t opts = {}
         );
